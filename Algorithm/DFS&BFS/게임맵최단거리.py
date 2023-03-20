1fc4436975c537de3097b72b8a745e06ea8e25c2
sunjung-Dev <sun17789@naver.com>
2023-03-20 15:03:40 +0900

    프로그래머스 게임 맵 최단거리

---
+++ Algorithm/DFS&BFS/게임맵최단거리.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+def bfs(x, y, maps):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    queue = deque()
+    queue.append((x, y))
+
+    while(queue):
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= len(maps) or ny >= len(maps[0]):
+                continue
+            if maps[nx][ny] == 0:
+                continue
+            if maps[nx][ny] == 1:
+                maps[nx][ny] = maps[x][y] + 1
+                queue.append((nx, ny))
+
+    if maps[-1][-1] == 1:
+        return -1
+
+    return maps[-1][-1]
+
+
+def solution(maps):
+    answer = 0
+    answer = bfs(0, 0, maps)
+    return answer
+
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
