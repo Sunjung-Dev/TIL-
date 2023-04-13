c1a760dd976d65df47e1e36a387883f3e8e996a7
sunjung-Dev <sun17789@naver.com>
2023-04-13 12:40:01 +0900

    프로그래머스, 최소직사각형.py

--- Algorithm/완전탐색/최소직사각형.py
+++ Algorithm/완전탐색/최소직사각형.py
@@ -1,29 +1,18 @@
 def solution(sizes):
     answer = 0
 
-    width_list = list()
-    length_list = list()
+    w = []
+    h = []
 
-    
     for i in range(len(sizes)):
-        width_list.append(sizes[i][0])
-        length_list.append(sizes[i][1])
-    
-    #새로와 가로의 최대값을 먼저 구하기 
-    width_list.sort(reverse=True)
-    length_list.sort(reverse=True)
+        if sizes[i][0] >= sizes[i][1]:
+            w.append(sizes[i][0])
+            h.append(sizes[i][1])
 
-    width = width_list[0] #가로
-    length = length_list[0] #세로 
-
-    for i in range(1, len(sizes)):
-        if width > length and length_list[i] > width:
-            length = length_list[i]
-        if length > width and width_list[i] > length:
-            width = width_list[i]
-
-    print(length, width)
-
+        else:
+            h.append(sizes[i][0])
+            w.append(sizes[i][1])
+    answer = max(w) * max(h)
     return answer
 
-print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
+print(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]))
