8e61bf98fc588342138cb0349397579e26c857bb
김선정 <sun177890@gmail.com>
2023-11-19 16:36:32 +0900

    Modify 프로그래머스-구명보트 알고리즘

--- Algorithm/Greedy/programmers_구명포트_greedy.py
+++ Algorithm/Greedy/programmers_구명포트_greedy.py
@@ -1,31 +1,20 @@
+from collections import deque
 def solution(people, limit):
     answer = 0    
-    cnt = 0 
     people.sort()
-    boat = list()
     result = list()
-    # for per in people:
-    #     print(per)
-    #     if (per < limit) and (sum(boat) + per <= limit):
-    #         boat.append(per)
-    #     else:
-    #         boat = list()
-    #         boat.append(per)
-    #     result.append(boat)
-    # print(result)
-
-    for i in range(len(people)-1, -1, -1):
-        if (people[i] < limit) and (sum(boat) + people[i] <= limit):
-            boat.append(people[i])
-            print("1", boat)
+    boat = list()
+    queue = deque(people)
+    while queue:
+        if len(queue) == 1:
+            answer += 1 
+            break
+        if queue[0] + queue[-1] <= limit:
+            queue.popleft()
+            queue.pop()
         else:
-            boat = list()
-            boat.append(people[i])
-            print("2", boat)
-            continue
-        result.append(boat)
-        # result.append(boat)
-    print(result)
+            queue.pop()
+        answer += 1
     return answer
 
-solution([70, 50, 80, 50], 100)
+print(solution([70, 80, 50, 50], 100))
